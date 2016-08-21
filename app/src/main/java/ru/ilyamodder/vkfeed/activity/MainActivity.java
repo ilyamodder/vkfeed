be1b5b325d7b05b7fda331e5dac11667845f713e
ilyamodder <ilyamodder@gmail.com>
2016-08-22 00:07:31 +0300

    Implemented style of MainActivity

--- app/src/main/java/ru/ilyamodder/vkfeed/activity/MainActivity.java
+++ app/src/main/java/ru/ilyamodder/vkfeed/activity/MainActivity.java
@@ -3,14 +3,22 @@ package ru.ilyamodder.vkfeed.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
-import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.VKSdk;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import ru.ilyamodder.vkfeed.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.recyclerView)
+    RecyclerView mRecyclerView;
 
     public static void start(Context context) {
         Intent intent = new Intent(context, MainActivity.class);
@@ -21,6 +29,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(mToolbar);
         if (!VKSdk.isLoggedIn()) {
             LoginActivity.startAsRootActivity(this);
         }
