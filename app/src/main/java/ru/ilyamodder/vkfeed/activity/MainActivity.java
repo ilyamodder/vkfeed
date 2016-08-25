0d33aadd571f2d2d3a727e8d1cd8a5ffa4b8a95b
ilyamodder <ilyamodder@gmail.com>
2016-08-25 19:02:47 +0300

    Added SwipeRefreshLayout

--- app/src/main/java/ru/ilyamodder/vkfeed/activity/MainActivity.java
+++ app/src/main/java/ru/ilyamodder/vkfeed/activity/MainActivity.java
@@ -3,6 +3,7 @@ package ru.ilyamodder.vkfeed.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -19,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     Toolbar mToolbar;
     @BindView(R.id.recyclerView)
     RecyclerView mRecyclerView;
+    @BindView(R.id.swipeRefreshLayout)
+    SwipeRefreshLayout mSwipeRefreshLayout;
 
     public static void start(Context context) {
         Intent intent = new Intent(context, MainActivity.class);
