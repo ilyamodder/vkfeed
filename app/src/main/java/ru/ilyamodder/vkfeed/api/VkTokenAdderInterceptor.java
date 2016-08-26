ed1cdd36e92df03e18b06b0357925a0146b3a4c5
ilyamodder <ilyamodder@gmail.com>
2016-08-26 21:12:15 +0300

    Fixed crash

--- app/src/main/java/ru/ilyamodder/vkfeed/api/VkTokenAdderInterceptor.java
+++ app/src/main/java/ru/ilyamodder/vkfeed/api/VkTokenAdderInterceptor.java
@@ -1,6 +1,7 @@
 package ru.ilyamodder.vkfeed.api;
 
 import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKSdk;
 
 import java.io.IOException;
 
@@ -17,10 +18,12 @@ public class VkTokenAdderInterceptor implements Interceptor {
     @Override
     public Response intercept(Chain chain) throws IOException {
         Request request = chain.request();
-        HttpUrl url = request.url().newBuilder()
-                .addQueryParameter("access_token", VKAccessToken.currentToken().accessToken)
-                .build();
-        request = request.newBuilder().url(url).build();
+        if (VKSdk.isLoggedIn()) {
+            HttpUrl url = request.url().newBuilder()
+                    .addQueryParameter("access_token", VKAccessToken.currentToken().accessToken)
+                    .build();
+            request = request.newBuilder().url(url).build();
+        }
         return chain.proceed(request);
     }
 }
