e22647269d48d3a6905626f07bf1e1a7d3682ac9
ilyamodder <ilyamodder@gmail.com>
2016-08-26 21:49:26 +0300

    Fixed the converter

--- app/src/main/java/ru/ilyamodder/vkfeed/rx/Converters.java
+++ app/src/main/java/ru/ilyamodder/vkfeed/rx/Converters.java
@@ -1,11 +1,13 @@
 package ru.ilyamodder.vkfeed.rx;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import ru.ilyamodder.vkfeed.model.Attachment;
 import ru.ilyamodder.vkfeed.model.Group;
 import ru.ilyamodder.vkfeed.model.Newsfeed;
+import ru.ilyamodder.vkfeed.model.NewsfeedItem;
 import ru.ilyamodder.vkfeed.model.Profile;
 import ru.ilyamodder.vkfeed.model.VKResponse;
 import ru.ilyamodder.vkfeed.model.local.JoinedPost;
@@ -18,34 +20,42 @@ import rx.Observable;
 public class Converters {
     public static Observable<List<JoinedPost>> toJoinedPost(Observable<VKResponse<Newsfeed>> source) {
         Observable<Newsfeed> serverData = source.map(VKResponse::getResponse);
-        return Observable.zip(serverData.map(Newsfeed::getItems).flatMap(Observable::from), serverData.map(Newsfeed::getProfiles),
-                serverData.map(Newsfeed::getGroups), (item, profiles, groups) -> {
-                    String name = "";
-                    String photoUrl = "";
-                    if (item.getSourceId() > 0) {
-                        for (Profile profile : profiles) {
-                            if (profile.getUserId() == item.getSourceId()) {
-                                name = profile.getFirstName() + " " + profile.getLastName();
-                                photoUrl = profile.getPhotoUrl();
-                                break;
+        return Observable.zip(serverData.map(Newsfeed::getItems), serverData.map(Newsfeed::getProfiles),
+                serverData.map(Newsfeed::getGroups), (items, profiles, groups) -> {
+                    List<JoinedPost> posts = new ArrayList<>();
+                    for (NewsfeedItem item : items) {
+                        String name = "";
+                        String photoUrl = "";
+                        if (item.getSourceId() > 0) {
+                            for (Profile profile : profiles) {
+                                if (profile.getUserId() == item.getSourceId()) {
+                                    name = profile.getFirstName() + " " + profile.getLastName();
+                                    photoUrl = profile.getPhotoUrl();
+                                    break;
+                                }
+                            }
+                        } else {
+                            for (Group group : groups) {
+                                if (group.getGroupId() == -1 * item.getSourceId()) {
+                                    name = group.getName();
+                                    photoUrl = group.getPhotoUrl();
+                                    break;
+                                }
                             }
                         }
-                    } else {
-                        for (Group group : groups) {
-                            if (group.getGroupId() == -1 * item.getSourceId()) {
-                                name = group.getName();
-                                photoUrl = group.getPhotoUrl();
-                                break;
-                            }
-                        }
+
+                        posts.add(new JoinedPost(item.getPostId(), name, new Date(item.getDate()),
+                                photoUrl, item.getText(), toPhotoUrls(item.getAttachments()),
+                                item.getLikesCount()));
                     }
-                    return new JoinedPost(item.getPostId(), name, new Date(item.getDate()),
-                            photoUrl, item.getText(), toPhotoUrls(item.getAttachments()),
-                            item.getLikesCount());
-                }).toList();
+                    return posts;
+                });
     }
 
     private static List<String> toPhotoUrls(List<Attachment> attachments) {
+        if (attachments == null) {
+            return new ArrayList<>();
+        }
         return Observable.from(attachments)
                 .filter(attachment -> attachment.getType().equals("photo"))
                 .map(attachment -> attachment.getPhoto().getPhoto604())
